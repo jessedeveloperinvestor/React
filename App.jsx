7a11817ad58a7fbe42427bc24499f51f5a98b162
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-06-04 14:51:55 -0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+		<a href='https://relaxed-dijkstra-f2b25b.netlify.app' class='nav__link_small'><strong>Jesse</strong> Hello World</a>
+		</div>
+    );
+  }
+}
+
+export default App;
